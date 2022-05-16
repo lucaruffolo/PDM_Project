322d7e111b97150cf94b34a11504392f5c4be12a
Luca <tbocyber@gmail.com>
2022-05-16 17:18:13 +0200

    Update
    Fix bottom menu
    Added right menu
    Start DarkLight mode

--- app/src/main/java/com/example/pdm_project/MainActivity.java
+++ app/src/main/java/com/example/pdm_project/MainActivity.java
@@ -1,21 +1,26 @@
 package com.example.pdm_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public Switch modeSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +32,33 @@ public class MainActivity extends AppCompatActivity {
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.firstFragment, R.id.secondFragment, R.id.thirdFragment).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        modeSwitch = (Switch) findViewById(R.id.modeDarkLight);
+   }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.right_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuRightItem:
+                showMessage("ITEM");
+                return true;
+            case R.id.menuRightInfo:
+                showMessage("INFO");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
     }
+
+    private void showMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
 }
